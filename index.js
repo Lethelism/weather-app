cdf28f550b06c20ea6907877b2f72a5ec795c0f7
Lethelism <MisterAnnual@ROLENTOHQ>
2019-10-04 13:58:22 -0400

    Adding files for project

---
+++ index.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = '4e604557e3a1442a71b812821ed12bbb';
+let city =  argv.c || 'atlanta';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+request(url, function(err, response, body) {
+	if(err){
+		console.log('error:', error);
+	}
+	else{
+		let weather = JSON.parse(body);
+		let message = `It is ${weather.main.temp} degrees in ${weather.name}!`;
+        console.log(message);
+	}
+});
